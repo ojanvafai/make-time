a6f1c9dd0d10c46cdd185c2b939ee674d026d1b4
Ojan Vafai <ojan@chromium.org>
2018-07-09 13:04:12 -0700

    Fix yearMonthDay to prefix with 0's so they can be sorted as strings. Also fix issue with adding 1 to the day incorreclty since days are 1-indexed unlike months.

--- static/MailProcessor.js
+++ static/MailProcessor.js
@@ -228,7 +228,16 @@ class MailProcessor {
 
   getYearMonthDay(timestamp) {
     let date = new Date(timestamp);
-    return date.getFullYear() + '/' + (date.getMonth() + 1) + '/' + (date.getDate() + 1);
+
+    let month = date.getMonth() + 1;
+    if (month < 10)
+      month = '0' + month;
+
+    let day = date.getDate();
+    if (day < 10)
+      day = '0' + day;
+
+    return date.getFullYear() + '/' + month + '/' + day;
   }
 
   async getSheetId(sheetName) {
