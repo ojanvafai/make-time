4aed86d45d068bdf0ba49b28d01ed190f32ae856
Ojan Vafai <ojan@chromium.org>
2018-08-02 19:24:36 -0700

    Add retry for gapi calls.

--- static/Net.js
+++ static/Net.js
@@ -1,4 +1,13 @@
 
-async function gapiFetch(method, requestParams, requestBody) {
-  return await method(requestParams, requestBody);
-};
+async function gapiFetch(method, requestParams, opt_requestBody) {
+  let numRetries = 3;
+  for (var i = 0; i < numRetries; i++) {
+    try {
+      return await method(requestParams, opt_requestBody);
+    } catch (e) {
+      console.log('Response failed.');
+      if (i == numRetries - 1)
+        throw e;
+    }
+  }
+}
