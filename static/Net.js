95d0092cf482da059e2b30206b0fe983bd1326c1
Ojan Vafai <ojan@chromium.org>
2018-08-07 21:56:26 -0700

    Resume requests started when offline and sleep between failures in the hopes of avoiding three immediate fialures in a row.

--- static/Net.js
+++ static/Net.js
@@ -1,13 +1,30 @@
 
+let queuedRequests_ = [];
+let TEN_SECONDS = 10 * 1000;
+
+function backOnline() {
+  return new Promise(resolve => queuedRequests_.push(resolve));
+}
+
+window.addEventListener('online', (e) => {
+  for (let request in queuedRequests_) {
+    request();
+  }
+  queuedRequests_ = [];
+});
+
 async function gapiFetch(method, requestParams, opt_requestBody) {
   let numRetries = 3;
   for (var i = 0; i < numRetries; i++) {
     try {
+      if (!navigator.onLine)
+        await backOnline();
       return await method(requestParams, opt_requestBody);
     } catch (e) {
       console.log('Response failed.');
       if (i == numRetries - 1)
         throw e;
+      await sleep(TEN_SECONDS);
     }
   }
 }
