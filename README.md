6b3f0108b1e78eaf9a501a724352dab218e5a6d6
Ojan Vafai <ojan@chromium.org>
2018-12-20 04:54:15 +0000

    Move deployment command to gulp.

--- README.md
+++ README.md
@@ -13,8 +13,11 @@ See https://stackoverflow.com/posts/30319507/revisions.
 For the dev server to work, you need to both start the firebase server and
 compile typescript after every change.
 
-$ cd make-time
+TODO: Create a gulp command that starts the server and the tsc --watch.
+
+```
 $ ./node_modules/firebase-tools/lib/bin/firebase.js serve --project mk-time
+```
 
 Now http://localhost:5000 serves make-time the same as consumer. For google.com
 credentials, use --port 5555 to serve from localhost:5555.
@@ -22,19 +25,31 @@ credentials, use --port 5555 to serve from localhost:5555.
 ## Compiling typescript
 You need to compile typescript anytime you modify a .ts file.
 
+// TODO: turn these into gulp commands.
+
 ### Compiling once
+```
 $ ./node_modules/.bin/tsc -p tsconfig.json
+```
 
 ### Setup a watch to compile any time a .ts file is modified
+```
 $ ./node_modules/.bin/tsc --watch -p tsconfig.json
+```
 
-### Deploying
-$ cd make-time
-$ ./deploy.py
+## Deploying
+```
+$ ./node_modules/gulp/bin/gulp.js deploy
+```
+
+### Or to only upload to the mk-time project:
+```
+$ ./node_modules/gulp/bin/gulp.js deploy --skip-google
+```
 
 In order to deploy, Ojan will need to make you a collaborator on the relevant appengine projects first.
 
-### Bundling
+## Bundling
 By default, running locally will serve unbundled and deploying will bundle.
 You can override the default behavior (locally and on the server) with the
 query parameter bundle=0 for no bundling and bundle=1 for bundling. For the
@@ -42,11 +57,11 @@ latter you'll need to manually run the command to generate the bundle.
 
 // TODO: Integrate gulp-watch or something like it to run gulp on file saves.
 
-Generating the bundle:
-$ cd make-time
+```
 $ ./node_modules/gulp/bin/gulp.js
+```
 
-Navigating the code:
+## Navigating the code
 index.html is the file that gets served, but it basically just loads main.js,
 which in turn loads everything else as ES Modules. Look at the onLoad() method
 in main.js to see how the page boots up or the router.add calls to see how the
--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,29 @@
-let gulp = require('gulp');
-let typescript = require('gulp-typescript');
-let sourcemaps = require('gulp-sourcemaps');
-let rollup = require('gulp-better-rollup');
-let uglify = require('gulp-uglify-es').default;
-let stripJsonComments = require('strip-json-comments');
-
 let fs = require('fs');
+let gulp = require('gulp');
+var gulpif = require('gulp-if');
+let md5 = require('md5');
+let rename = require('gulp-rename');
+let replace = require('gulp-string-replace');
+let rollup = require('gulp-better-rollup');
+let shell = require('gulp-shell')
+let sourcemaps = require('gulp-sourcemaps');
+let stripJsonComments = require('strip-json-comments');
+let typescript = require('gulp-typescript');
+let uglify = require('gulp-uglify-es').default;
+
 let tsConfig = JSON.parse(stripJsonComments(fs.readFileSync('./tsconfig.json', 'utf8')));
 let outDir = tsConfig.compilerOptions.outDir;
+let mainFilename = '/main.js';
+let bundleDir = '/bundle';
 
-gulp.task("delete-out-dir", function(cb) {
+gulp.task("delete", (callback) => {
   var rimraf = require('rimraf');
   rimraf.sync(outDir);
-  cb();
+  callback();
 });
 
-gulp.task("compile", function () {
+gulp.task("compile", () => {
   let project = typescript.createProject("tsconfig.json");
-
   return project.src()
     // Even though the sourcemaps for the .ts files don't work in the bundled
     // version, still generate them so that you can debug the .ts files when
@@ -32,13 +38,63 @@ gulp.task("compile", function () {
 // TODO: Figure out why loadMaps:true isn't pulling in the sourcemaps generated
 // by the typescript compile step.
 gulp.task('bundle', () => {
-  return gulp.src('public/gen/main.js')
+  return gulp.src(outDir + mainFilename)
     .pipe(sourcemaps.init({loadMaps: true}))
     .pipe(rollup({}, 'esm'))
     .pipe(uglify())
     // save sourcemap as separate file (in the same folder)
     .pipe(sourcemaps.write(''))
-    .pipe(gulp.dest('public/gen/bundle'))
+    .pipe(gulp.dest(outDir + bundleDir));
 })
 
-gulp.task('default', gulp.series('delete-out-dir', 'compile', 'bundle'));
+gulp.task('upload', () => {
+  let checksumKeyword = "-checksum-";
+
+  // Append md5 checksum to gen/bundle/main.js and it's sourcemap.
+  let bundleMain = outDir + bundleDir + mainFilename;
+  let checksum = md5(fs.readFileSync(bundleMain, 'utf8'));
+  gulp.src([bundleMain, bundleMain + '.map'])
+      .pipe(rename(function (path) {
+        let parts = path.basename.split('.');
+        path.basename = parts[0] + checksumKeyword + checksum;
+        if (parts.length == 2)
+          path.basename += '.' + parts[1];
+      }))
+      .pipe(gulp.dest(outDir + bundleDir));
+
+  // Append md5 checksum to maifest.json.
+  let manifestChecksum = md5(fs.readFileSync(bundleMain, 'utf8'));
+  gulp.src('public/manifest.json')
+      .pipe(rename(function (path) {
+        path.basename += checksumKeyword + manifestChecksum;
+      }))
+      .pipe(gulp.dest(outDir));
+
+  pathsToRewrite = [
+    ['/gen/bundle/main.js', `/gen/bundle/main${checksumKeyword}${checksum}.js`],
+    ['./manifest.json', `./gen/manifest${checksumKeyword}${manifestChecksum}.json`],
+  ];
+
+  // TODO: Find a way to avoid rewriting index.html in place while still
+  // having firebase serve up index.html without needing a build step for
+  // unbundled local development.
+  gulp.src(['public/index.html'])
+    .pipe(replace(pathsToRewrite[0][0], pathsToRewrite[0][1]))
+    .pipe(replace(pathsToRewrite[1][0], pathsToRewrite[1][1]))
+    .pipe(gulp.dest('public'))
+
+  let skipGoogle = process.argv.includes('--skip-google');
+
+  let firebaseDeploy = './node_modules/firebase-tools/lib/bin/firebase.js deploy --project ';
+  return gulp.src(['public/index.html'])
+    .pipe(shell([firebaseDeploy + 'mk-time']))
+    .pipe(gulpif(!skipGoogle, shell([firebaseDeploy + 'google.com:mktime'])))
+    .pipe(replace(pathsToRewrite[0][1], pathsToRewrite[0][0]))
+    .pipe(replace(pathsToRewrite[1][1], pathsToRewrite[1][0]))
+    .pipe(gulp.dest('public'))
+});
+
+// TODO also deploy to google.com:mktime
+gulp.task('fresh-bundle', gulp.series('delete', 'compile', 'bundle'));
+gulp.task('default', gulp.series('fresh-bundle'));
+gulp.task('deploy', gulp.series('fresh-bundle', 'upload'));
