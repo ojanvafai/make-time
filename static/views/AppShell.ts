1c935d9590da4d4d54b022c101fda287605061d0
Ojan Vafai <ojan@chromium.org>
2019-03-03 10:51:11 -0800

    Move all the app shell DOM into AppShell.

---
+++ static/views/AppShell.ts
@@ -0,0 +1,246 @@
+import {showHelp} from '../BaseMain.js';
+import {COMPLETED_EVENT_NAME, RadialProgress} from '../RadialProgress.js';
+
+let progressElements: Map<string, RadialProgress> = new Map();
+let titleStack_: TitleEntry[] = [];
+let loaderTitleStack_: TitleEntry[] = [];
+
+interface TitleEntry {
+  key: string;
+  title: (HTMLElement|string)[];
+}
+
+let CURRENT_PAGE_CLASS = 'current-page';
+let DRAWER_OPEN = 'drawer-open';
+
+export class BackEvent extends Event {
+  static NAME = 'back';
+  constructor() {
+    super(BackEvent.NAME);
+  }
+}
+
+export class AppShell extends HTMLElement {
+  private drawer_: HTMLElement;
+  private mainContent_: HTMLElement;
+  private content_: HTMLElement;
+  private backArrow_: HTMLElement;
+  private menuToggle_: SVGSVGElement;
+  private subject_: HTMLElement;
+  // TODO: Make these not static.
+  private static title_: HTMLElement;
+  private static loader_: HTMLElement;
+  private static footer_: HTMLElement;
+
+  constructor() {
+    super();
+    this.drawer_ = document.createElement('div');
+    this.drawer_.id = 'drawer';
+    this.drawer_.className = 'panel';
+
+    this.mainContent_ = document.createElement('div');
+    this.mainContent_.id = 'main-content';
+    this.mainContent_.className = 'panel';
+
+    document.body.append(this.drawer_, this.mainContent_);
+
+    let toolbar = document.createElement('div');
+    toolbar.id = 'toolbar';
+    this.content_ = document.createElement('div');
+    this.content_.id = 'content';
+    AppShell.footer_ = document.createElement('div');
+    AppShell.footer_.id = 'footer';
+    this.mainContent_.append(toolbar, this.content_, AppShell.footer_);
+
+    this.backArrow_ = document.createElement('div');
+    this.backArrow_.id = 'back-arrow';
+    this.backArrow_.className = 'menu-open-button';
+    this.backArrow_.style.display = 'none';
+    this.backArrow_.textContent = '⬅';
+
+    this.menuToggle_ =
+        document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+    this.menuToggle_.id = 'hamburger-menu-toggle';
+    this.menuToggle_.classList.add('menu-open-button');
+    this.menuToggle_.setAttribute('viewBox', '0 0 32 32');
+    let path = document.createElementNS('http://www.w3.org/2000/svg', 'path');
+    path.setAttribute(
+        'd',
+        `M4,10h24c1.104,0,2-0.896,2-2s-0.896-2-2-2H4C2.896,6,2,6.896,2,8S2.896,10,4,10z M28,14H4c-1.104,0-2,0.896-2,2  s0.896,2,2,2h24c1.104,0,2-0.896,2-2S29.104,14,28,14z M28,22H4c-1.104,0-2,0.896-2,2s0.896,2,2,2h24c1.104,0,2-0.896,2-2  S29.104,22,28,22z`);
+    this.menuToggle_.append(path);
+
+    this.subject_ = document.createElement('div');
+    this.subject_.id = 'subject';
+    AppShell.title_ = document.createElement('div');
+    AppShell.title_.id = 'title';
+    AppShell.loader_ = document.createElement('div');
+    AppShell.loader_.id = 'loader';
+
+    toolbar.append(
+        this.backArrow_, this.menuToggle_, AppShell.title_, this.subject_,
+        AppShell.loader_);
+
+    this.appendMenu_();
+    this.menuToggle_.addEventListener('click', (e) => {
+      e.stopPropagation();
+      this.toggleMenu();
+    });
+
+    this.mainContent_.addEventListener('click', (e) => {
+      if (this.mainContent_.classList.contains(DRAWER_OPEN)) {
+        e.preventDefault();
+        this.closeMenu();
+      }
+    });
+
+    this.backArrow_.addEventListener(
+        'click', () => this.dispatchEvent(new BackEvent()));
+  }
+
+  static setFooter(dom?: HTMLElement) {
+    AppShell.footer_.textContent = '';
+    if (dom)
+      AppShell.footer_.append(dom);
+  }
+
+  static addToFooter(dom: HTMLElement) {
+    AppShell.footer_.append(dom);
+  }
+
+  static updateTitle(key: string, ...opt_title: string[]) {
+    AppShell.updateTitleBase(titleStack_, AppShell.title_, key, ...opt_title);
+  }
+
+  static updateLoaderTitle(
+      key: string, count: number, ...opt_title: (HTMLElement|string)[]) {
+    let progress = progressElements.get(key);
+    if (!progress) {
+      progress = new RadialProgress();
+      progressElements.set(key, progress);
+      progress.addEventListener(COMPLETED_EVENT_NAME, () => {
+        AppShell.clearLoaderTitle(key);
+      });
+    }
+
+    progress.addToTotal(count);
+
+    AppShell.updateTitleBase(
+        loaderTitleStack_, AppShell.loader_, key, ...opt_title, progress);
+    return progress;
+  }
+
+  static clearLoaderTitle(key: string) {
+    AppShell.updateTitleBase(loaderTitleStack_, AppShell.loader_, key);
+  }
+
+  static updateTitleBase(
+      stack: TitleEntry[], node: HTMLElement, key: string,
+      ...opt_title: (HTMLElement|string)[]) {
+    let index = stack.findIndex((item) => item.key == key);
+    if (!opt_title[0]) {
+      if (index != -1)
+        stack.splice(index, 1);
+    } else if (index == -1) {
+      stack.push({
+        key: key,
+        title: opt_title,
+      });
+    } else {
+      let entry = stack[index];
+      entry.title = opt_title;
+    }
+
+    node.textContent = '';
+    if (stack.length)
+      node.append(...stack[stack.length - 1].title);
+  }
+
+
+  setContent(newContent: string|Node) {
+    this.content_.textContent = '';
+    this.content_.append(newContent);
+  }
+
+  setSubject(...items: (string|Node)[]) {
+    this.subject_.textContent = '';
+    this.subject_.append(...items);
+  }
+
+  get contentScrollTop() {
+    return this.content_.scrollTop;
+  }
+
+  set contentScrollTop(value: number) {
+    this.content_.scrollTop = value;
+  }
+
+  showMenuButton(hide?: boolean) {
+    this.menuToggle_.style.visibility = hide ? 'hidden' : 'visible';
+  }
+
+  showBackArrow(show: boolean) {
+    this.menuToggle_.style.display = show ? 'none' : '';
+    this.backArrow_.style.display = show ? '' : 'none';
+  }
+
+  private createMenuItem_(name: string, options: any) {
+    let a = document.createElement('a');
+    a.append(name);
+    a.className = 'item';
+
+    if (options.nested)
+      a.classList.add('nested');
+
+    if (options.href)
+      a.href = options.href;
+
+    if (options.onclick)
+      a.onclick = options.onclick;
+
+    a.addEventListener('click', () => this.closeMenu());
+    return a;
+  }
+
+  private appendMenu_() {
+    let helpButton = this.createMenuItem_('Help', {
+      onclick: async () => showHelp(),
+    });
+
+    let menuTitle = document.createElement('div');
+    menuTitle.append('MakeTime phases');
+
+    this.drawer_.append(
+        menuTitle,
+        this.createMenuItem_('Compose', {href: '/compose', nested: true}),
+        this.createMenuItem_('Triage', {href: '/triage', nested: true}),
+        this.createMenuItem_('Todo', {href: '/todo', nested: true}),
+        this.createMenuItem_('Calendar (alpha)', {href: '/calendar'}),
+        this.createMenuItem_('Settings', {href: '/settings'}), helpButton);
+  }
+
+  private openMenu() {
+    let menuItems =
+        <NodeListOf<HTMLAnchorElement>>this.drawer_.querySelectorAll('a.item');
+    for (let item of menuItems) {
+      if (item.pathname == location.pathname) {
+        item.classList.add(CURRENT_PAGE_CLASS);
+      } else {
+        item.classList.remove(CURRENT_PAGE_CLASS);
+      }
+    }
+
+    this.mainContent_.classList.add(DRAWER_OPEN);
+  }
+
+  private closeMenu() {
+    this.mainContent_.classList.remove(DRAWER_OPEN);
+  }
+
+  private toggleMenu() {
+    if (this.mainContent_.classList.contains(DRAWER_OPEN))
+      this.closeMenu();
+    else
+      this.openMenu();
+  }
+}
+window.customElements.define('mk-appshell', AppShell);
