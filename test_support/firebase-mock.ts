0e6175d9d1ae2c9666d1d63e07ee0e9ad845e509
Tim Dresser <tdresser@gmail.com>
2019-05-05 11:54:57 -0400

    Improving tests, need to use actual firebase.

--- test_support/firebase-mock.ts
+++ test_support/firebase-mock.ts
@@ -1,26 +1,48 @@
-export const firebase = {
-  firestore: function() {
+class Collection {
+  onSnapshot(_onNext: any): any {
+    // This is overridden per test.
+  }
+  orderBy() {
+    return this;
+  }
+
+  doc(): any {
     return {
-      collection: function(_uid: Number) {
-        return {
-          doc: function() {
-            return {
-              collection: function() {
-                return {
-                  onSnapshot: function(_onNext: any) {
-                    // This is defined per test.
-                  }, orderBy: function() {
-                    return this;
-                  }
-                }
-              }
-            }
-          }
-        };
+      collection: () => {
+        return firebase.firestore().collection(0);
+      }, update: () => {
+
       }
-    };
-  },
-  auth: function() {
+    }
+  }
+}
+
+class Firebase {
+  constructor() {
+    // This is weird - FieldValue lives on the method |firestore|.
+    // @ts-ignore
+    this.firestore.FieldValue = {delete: () => {}};
+  }
+  firestore() {
+    return this.firestore_;
+  }
+
+  auth() {
     return {currentUser: {uid: 1}};
   }
-};
+
+  update() {}
+
+  private firestore_ = new Firestore();
+}
+
+class Firestore {
+  constructor() {}
+  collection(_uid: Number): any {
+    return this.collection_;
+  };
+
+  private collection_ = new Collection();
+}
+
+export const firebase = new Firebase();
